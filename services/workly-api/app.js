e0c7813ce6fe20b46b99ed80d2ce8ffe50780a75
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2022-01-24 22:12:55 -0500

    feat: configured apollo graphql server and prisma in express
    - Installed Prisma
    - Installed Apollo Express Server
    - Configured apollo-server-express package to be compatible with express generated template
    - Configured Prisma

--- services/workly-api/app.js
+++ services/workly-api/app.js
@@ -2,9 +2,38 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { ApolloServer } = require('apollo-server-express');
+const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+const typeDefs =`
+type Query{
+    hello: String!
+    users: [User]
+}
+`,
+    resolvers = {
+        Query: {
+            hello: () => "Hello",
+        }
+    };
+async function startApolloServer(app, httpServer) {
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
+    });
+
+
+    await server.start();
+    server.applyMiddleware({
+        app
+    });
+
+    return server;
+}
 
 var app = express();
 
@@ -17,4 +46,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-module.exports = app;
+module.exports = { app, startApolloServer };
